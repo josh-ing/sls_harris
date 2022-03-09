cddcae0f596f93ddcc4b69672a1c814784307cf7
Joshua <joshuang21@gmail.com>
2022-03-09 11:19:11 -0500

    omg this opencv library is going to be the death of me

--- sift.py
+++ sift.py
@@ -1,7 +1,8 @@
+import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2 as cv
 
-filename = "wisconsin_flat"
-img = cv.imread(filename)
+img = cv.imread('wisconsin_flat')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+sift = cv.xfeatures2d.SIFT_create()
