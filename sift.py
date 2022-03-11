1fc7073665814adeb472d1675111995fb0c0f6a6
Joshua <joshuang21@gmail.com>
2022-03-11 12:47:40 -0500

    i think i broke the alignment method

--- sift.py
+++ sift.py
@@ -1,25 +1,42 @@
 import cv2 as cv
+from cv2 import MOTION_HOMOGRAPHY
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
+
+MAX_FEATURES = 500
+GOOD_MATCH_PERCENT = 0.15
 
 img1 = cv.imread('C:\sls_harris\data\DSCF0281.jpg')
+imgcolor = cv.imread('C:\sls_harris\data\DSCF0281.jpg')
 img2 = cv.imread('C:\sls_harris\data\wisconsin_flat.jpg')
 img1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
 img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+img2 = cv.resize(img2, (480, 640))
+height, width = img2.shape
 
-sift = cv.SIFT_create()
+#transformecc helps us calculate rotation of two images and aligns them, might be what we want
+sift = cv.SIFT_create(MAX_FEATURES)
 
 keypoints1, descript1 = sift.detectAndCompute(img1, None)
 keypoints2, descript2 = sift.detectAndCompute(img2, None)
-#might switch to knn based sift so I can print coordinates 
-#perhaps apply ratio test to find coordinates of all keypoints that are matched
-bf = cv.BFMatcher_create(cv.NORM_L1, crossCheck = True)
-# bf = cv.BFMatcher()
-# matches = bf.knnMatch(descript1, descript2, k=2)
+
+#match features of two images
+bf = cv.BFMatcher(cv.NORM_L1, crossCheck = True)
 matches = bf.match(descript1, descript2)
-#ratio test, print this later
-matches = sorted(matches, key = lambda x:x.distance)
+matches = sorted(matches, key = lambda x:x.distance, reverse= False)
 img3 = cv.drawMatches(img1, keypoints1, img2, keypoints2, matches[:50], None, flags = 2)
-# img3 = cv.drawMatchesKnn(img1, keypoints1, img2, keypoints2, matches, None,**draw_params)
-# plt.imshow(cv.cvtColor(img3, cv.COLOR_BGR2RGB))
 plt.imshow(img3), plt.show()
+
+#now here's the fun part
+p1 = np.zeros((len(matches), 2))
+p2 = np.zeros((len(matches), 2))
+
+for i in range(len(matches)):
+    p1[i, :] = keypoints1[matches[i].queryIdx].pt
+    p2[i, :] = keypoints2[matches[i].queryIdx].pt
+
+homography, mask = cv.findHomography(p1, p2, cv.RANSAC)
+
+transformed_img = cv.warpPerspective(imgcolor, homography, (width, height))
+plt.imshow(transformed_img)
