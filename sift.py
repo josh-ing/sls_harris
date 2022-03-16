96b7ddba833d0ed01d3f2af762a5e87a4c2ef6d9
Joshua Ng <joshuang21@gmail.com>
2022-03-16 14:13:05 -0400

    might change approach to homography instead of sift

--- sift.py
+++ sift.py
@@ -3,15 +3,20 @@ from cv2 import MOTION_HOMOGRAPHY
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import os
 
 MAX_FEATURES = 5000
 GOOD_MATCH_PERCENT = 0.15
+#maybe instead use homography transformation instead of sift, because sift is for image recognition
 
-img1 = cv.imread('C:\sls_harris\data\DSCF0285.jpg')
-img2 = cv.imread('C:\sls_harris\data\wisconsin_flat.jpg')
+directory = os.getcwd()
+img1 = cv.imread(os.path.join(directory, 'data\DSCF0276.jpg'))
+img2 = cv.imread(os.path.join(directory, 'data\wisconsin_flat.jpg'))
 img1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
 img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
 img2 = cv.resize(img2, (480, 640))
+
+# h, status = cv.findHomography(pts_src, pts_dst)
 
 #transformecc helps us calculate rotation of two images and aligns them, might be what we want
 sift = cv.SIFT_create()
