a9d43c790aaefc18393a34c7567a9515bc359f35
Joshua <joshuang21@gmail.com>
2022-03-23 14:49:24 -0400

    uhhhhhh it kinda works? it kinda doesn't? depends on the logo, might change from feature based to edge detection/corner based

--- app.py
+++ app.py
@@ -2,13 +2,26 @@ import os
 import cv2 as cv
 from feature_match import align_images
 
+#maybe instead of using orb feature matching, use sobel edge detection or canny
+#or use harris corner detection
+#feature matches do be kinda not working for complex logos and images
+#instead sobel edge detection/ corner detection may be the way to go
+# try this https://stackoverflow.com/questions/54589566/how-to-detect-edge-points-of-an-object-using-opencv-python
 
 directory = os.getcwd()
-input = os.path.join(directory, 'data\IMG_3627_IG.jpg')
-reference = os.path.join(directory, 'data\\template_wisconsin.jpg')
+input = os.path.join(directory, 'data\IMG_3788.jpg')
+reference = os.path.join(directory, 'data\\falcons_reference.jpg')
 image = cv.imread(input, cv.IMREAD_COLOR)
 template = cv.imread(reference, cv.IMREAD_COLOR)
 
-aligned, h = align_images(image, template, debug=True)
+x, y, z = image.shape
+i, j, k = template.shape
+if ((x, y) < (i, j)):
+    image = cv.resize(image, (i,j))
+if ((x, y) > (i, j)):
+    template = cv.resize(template, (x, y))
+
+aligned, h, img3 = align_images(image, template)
 cv.imwrite("aligned.jpg", aligned)
+cv.imwrite("image_matches.jpg", img3)
 print("Estimated homography : \n", h)
--- feature_match.py
+++ feature_match.py
@@ -3,33 +3,37 @@ from cv2 import DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import math
 
-MAX_FEATURES = 500
+
+MAX_FEATURES = 350
 GOOD_MATCH_PERCENT = 0.2
-#maybe instead use homography transformation instead of sift, because sift is for image recognition
 
-def align_images(input, reference, debug=False):
-    input = cv.cvtColor(input, cv.COLOR_BGR2GRAY)
-    reference = cv.cvtColor(reference, cv.COLOR_BGR2GRAY)
-    # h, status = cv.findHomography(pts_src, pts_dst)
+def align_images(input, reference):
+    inputgray = cv.cvtColor(input, cv.COLOR_BGR2GRAY)
+    referencegray = cv.cvtColor(reference, cv.COLOR_BGR2GRAY)
 
     #transformecc helps us calculate rotation of two images and aligns them, might be what we want
     orb = cv.ORB_create(MAX_FEATURES)
+    # sift = cv.SIFT_create(MAX_FEATURES)
 
-    keypoints1, descript1 = orb.detectAndCompute(input, None)
-    keypoints2, descript2 = orb.detectAndCompute(reference, None)
+    # keypoints1, descript1 = sift.detectAndCompute(input, None)
+    # keypoints2, descript2 = sift.detectAndCompute(reference, None)
+    keypoints1, descript1 = orb.detectAndCompute(inputgray, None)
+    keypoints2, descript2 = orb.detectAndCompute(referencegray, None)
 
     #match features of two images
 
     bf = cv.DescriptorMatcher_create(DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    # bf = cv.BFMatcher_create(cv.NORM_L1, crossCheck = True)
     matches = bf.match(descript1, descript2, None)
-    matches = sorted(matches, key = lambda x: x.distance)
+    matches = sorted(matches, key = lambda x: x.distance, reverse=False)
 
-    good_matches = int(len(matches) * GOOD_MATCH_PERCENT)
-    matches = matches[:good_matches]
+    # good_matches = int(len(matches) * GOOD_MATCH_PERCENT)
+    # matches = matches[:good_matches]
+    matches = matches[:int(len(matches)* GOOD_MATCH_PERCENT)]
 
-    img3 = cv.drawMatches(input, keypoints1, reference, keypoints2, matches, None, flags = 2)
-    plt.imshow(img3), plt.show()
+    img3 = cv.drawMatches(inputgray, keypoints1, referencegray, keypoints2, matches, None, flags = 2)
 
     #homography matrix
     pointsA = np.zeros((len(matches), 2), dtype=np.float32)
@@ -38,12 +42,12 @@ def align_images(input, reference, debug=False):
     for (i, m) in enumerate(matches):
         pointsA[i, :] = keypoints1[m.queryIdx].pt
         pointsB[i, :] = keypoints2[m.trainIdx].pt
-
-    (H, mask) = cv.findHomography(pointsA, pointsB, cv.RANSAC)
-    height, width = reference.shape
+    
+    (H, mask) = cv.findHomography(pointsA, pointsB, method=cv.RANSAC)
+    (height, width) = reference.shape[:2]
     aligned = cv.warpPerspective(input, H, (width, height))
 
-    return aligned, H
+    return aligned, H, img3
 
     
 
