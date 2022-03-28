fa7b2e2010f10d2d77314da0b4daef7497c278ce
Joshua Ng <joshuang21@gmail.com>
2022-03-28 14:55:03 -0400

    idk why corner harris isn't working, i need to fix this later

--- app_corner.py
+++ app_corner.py
@@ -1,3 +1,9 @@
 import os
 import cv2 as cv
-from harris_corner import find_corners, find_centroids
+from harris_corner import find_corners
+
+directory = os.getcwd()
+input = os.path.join(directory, 'data\IMG_3631_IG.jpg')
+image = cv.imread(input, cv.IMREAD_COLOR)
+corners = find_corners(image)
+cv.imwrite('harris_corner.jpg', corners)
--- harris_corner.py
+++ harris_corner.py
@@ -6,25 +6,27 @@ from PIL import Image
 import math
 
 def find_corners(image):
-    
+    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     gray = np.float32(image)
-    dst = cv.cornerHarris(gray, 2, 3, 0.04)
+    dst = cv.cornerHarris(gray, 2, 5, 0.07)
     dst = cv.dilate(dst, None)
+    image[dst > 0.01 * dst.max()]=[0, 0, 255]
+    return image
 
 
-    #list of corners
-    corners_input = find_centroids(dst)
+    # #list of corners
+    # corners_input = find_centroids(dst)
 
 
-def find_centroids(image):
-    ret, dest = cv.threshold(image, 0.01 * image.max(), 255, 0)
-    dest = np.uint8(dest)
+# def find_centroids(image):
+#     ret, dest = cv.threshold(image, 0.01 * image.max(), 255, 0)
+#     dest = np.uint8(dest)
 
-    # find centroids
-    ret, labels, stats, centroids = cv.connectedComponentsWithStats(image)
-    # define the criteria to stop and refine the corners
-    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 
-                0.001)
-    corners = cv.cornerSubPix(image,np.float32(centroids),(5,5), 
-              (-1,-1),criteria)
-    return corners
+#     # find centroids
+#     ret, labels, stats, centroids = cv.connectedComponentsWithStats(image)
+#     # define the criteria to stop and refine the corners
+#     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 
+#                 0.001)
+#     corners = cv.cornerSubPix(image,np.float32(centroids),(5,5), 
+#               (-1,-1),criteria)
+#     return corners
