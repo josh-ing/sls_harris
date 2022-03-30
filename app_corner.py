1bd480cb555852c430706359d75161203f6c0ab2
Joshua Ng <joshuang21@gmail.com>
2022-03-30 14:33:37 -0400

    i don't think harris is the way to go

--- app_corner.py
+++ app_corner.py
@@ -3,7 +3,7 @@ import cv2 as cv
 from harris_corner import find_corners
 
 directory = os.getcwd()
-input = os.path.join(directory, 'data\IMG_3631_IG.jpg')
+input = os.path.join(directory, 'data\\titans_reference.jpg')
 image = cv.imread(input, cv.IMREAD_COLOR)
 corners = find_corners(image)
 cv.imwrite('harris_corner.jpg', corners)
--- harris_corner.py
+++ harris_corner.py
@@ -6,27 +6,27 @@ from PIL import Image
 import math
 
 def find_corners(image):
-    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    gray = np.float32(image)
-    dst = cv.cornerHarris(gray, 2, 5, 0.07)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    gray = np.float32(gray)
+    dst = cv.cornerHarris(gray, 2, 3, 0.04)
     dst = cv.dilate(dst, None)
-    image[dst > 0.01 * dst.max()]=[0, 0, 255]
+    ret, dst = cv.threshold(dst, 0.01 * dst.max(), 255, 0)
+    # image[dst > 0.01 * dst.max()]=[0, 0, 255]
+
+    dst = np.uint8(dst)
+
+    # find centroids
+    ret, labels, stats, centroids = cv.connectedComponentsWithStats(dst)
+    # define the criteria to stop and refine the corners
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 
+                0.001)
+    corners = cv.cornerSubPix(gray,np.float32(centroids),(5,5), 
+              (-1,-1),criteria)
+
+    res = np.hstack((centroids,corners))
+    res = np.int0(res)
+    image[res[:,1],res[:,0]]=[0,0,255]
+    image[res[:,3],res[:,2]] = [0,255,0]
     return image
 
 
-    # #list of corners
-    # corners_input = find_centroids(dst)
-
-
-# def find_centroids(image):
-#     ret, dest = cv.threshold(image, 0.01 * image.max(), 255, 0)
-#     dest = np.uint8(dest)
-
-#     # find centroids
-#     ret, labels, stats, centroids = cv.connectedComponentsWithStats(image)
-#     # define the criteria to stop and refine the corners
-#     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 
-#                 0.001)
-#     corners = cv.cornerSubPix(image,np.float32(centroids),(5,5), 
-#               (-1,-1),criteria)
-#     return corners
