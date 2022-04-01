389190ab72a3248150e1e0f2fd2b000341e34276
Joshua <joshuang21@gmail.com>
2022-04-01 13:14:51 -0400

    trying new approach

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from feature_match import align_images
 # try this https://stackoverflow.com/questions/54589566/how-to-detect-edge-points-of-an-object-using-opencv-python
 
 directory = os.getcwd()
-input = os.path.join(directory, 'data\IMG_3782.jpg')
-reference = os.path.join(directory, 'data\steelers_reference.jpg')
+input = os.path.join(directory, 'data\IMG_3786.jpg')
+reference = os.path.join(directory, 'data\\falcons_reference.jpg')
 image = cv.imread(input, cv.IMREAD_COLOR)
 template = cv.imread(reference, cv.IMREAD_COLOR)
 
@@ -25,3 +25,8 @@ aligned, h, img3 = align_images(image, template)
 cv.imwrite("aligned.jpg", aligned)
 cv.imwrite("image_matches.jpg", img3)
 print("Estimated homography : \n", h)
+
+#use segmenation
+#color mask
+#make it binary, no need to match features inside logo, just mask anything that's not logo and get outline
+#use homography to match that
